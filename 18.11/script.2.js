c6952ebecbf67824a0c095acfb3416398f33a88a
Riccardo V. <ricc.vargiu@gmail.com>
2025-11-18 13:12:21 +0100

    fix:script

--- 18.11/script.2.js
+++ 18.11/script.2.js
@@ -1,14 +1,14 @@
-// const tuttiGliA = document.querySelectorAll("a");
-// console.log(tuttiGliA);
+const tuttiGliA = document.querySelectorAll("a");
+console.log(tuttiGliA);
 
-// const tuttiParagrafi = document.querySelectorAll("p");
-// console.log(tuttiParagrafi.length);
+const tuttiParagrafi = document.querySelectorAll("p");
+console.log(tuttiParagrafi.length);
 
-// const tuttiDataId = document.querySelectorAll("[data-id]");
-// console.log(tuttiDataId);
+const tuttiDataId = document.querySelectorAll("[data-id]");
+console.log(tuttiDataId);
 
-// const tuttiCancelletto = document.querySelectorAll("a[href='#']");
-// console.log(tuttiCancelletto);
+const tuttiCancelletto = document.querySelectorAll("a[href='#']");
+console.log(tuttiCancelletto);
 
 // const contenuto = document.querySelector("#contenuto");
 // console.log(contenuto);
