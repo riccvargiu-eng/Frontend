dbf2d39fe2aafd2132d5fe3ab3020245adba154a
Riccardo V. <ricc.vargiu@gmail.com>
2025-11-18 13:11:39 +0100

    Refactor script.2.js to comment out unused DOM queries and enhance readability

--- 18.11/script.2.js
+++ 18.11/script.2.js
@@ -1,11 +1,34 @@
-const tuttiGliA = document.querySelectorAll("a");
-console.log(tuttiGliA);
+// const tuttiGliA = document.querySelectorAll("a");
+// console.log(tuttiGliA);
 
-const tuttiParagrafi = document.querySelectorAll("p");
-console.log(tuttiParagrafi.length);
+// const tuttiParagrafi = document.querySelectorAll("p");
+// console.log(tuttiParagrafi.length);
 
-const tuttiDataId = document.querySelectorAll("[data-id]");
-console.log(tuttiDataId);
+// const tuttiDataId = document.querySelectorAll("[data-id]");
+// console.log(tuttiDataId);
 
-const tuttiCancelletto = document.querySelectorAll("a[href='#']");
-console.log(tuttiCancelletto);
+// const tuttiCancelletto = document.querySelectorAll("a[href='#']");
+// console.log(tuttiCancelletto);
+
+// const contenuto = document.querySelector("#contenuto");
+// console.log(contenuto);
+// //trovare genitore
+// const genitore = contenuto.parentElement;
+// console.log(contenuto.parentElement);
+// //trovare figlio
+// const contenuto = document.querySelector("#contenuto");
+// console.log(contenuto.parentSibling);
+
+// //trovare fratello
+// const contenuto = document.querySelector("#contenuto");
+// console.log();
+
+//trovare genitore
+const genitore = document.querySelector("#contenuto");
+console.log("Genitore:", genitore.parentElement);
+//trovare figlio
+const figlio = document.querySelector("#contenuto");
+console.log("Figlio:", figlio.children);
+//trovare fratello
+const fratello = document.querySelector("#contenuto");
+console.log("Fratello:", fratello.children);
