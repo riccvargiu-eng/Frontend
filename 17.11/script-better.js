116d4b95815852399c46874d25ecaceeb5993347
Riccardo V. <ricc.vargiu@gmail.com>
2025-11-18 19:03:45 +0100

    fix:script-better 17.11

--- 17.11/script-better.js
+++ 17.11/script-better.js
@@ -1,1 +1,42 @@
-const calcolo 
+// function calcolo(tipo) {
+//   const n1 = Number(document.getElementById("n1").value);
+//   const n2 = Number(document.getElementById("n2").value);
+
+//   let risultato;
+
+//   if (tipo === "somma") {
+//     risultato = n1 + n2;
+//   } else if (tipo === "sottrazione") {
+//     risultato = n1 - n2;
+//   } else if (tipo === "divisione") {
+//     risultato = n1 / n2;
+//   } else if (tipo === "moltiplicazione") {
+//     risultato = n1 * n2;
+//   }
+
+//   document.getElementById("Risultato").textContent = "Risultato " + risultato;
+// }
+
+function calcolo(tipo) {
+    const n1 = Number(document.getElementById("n1").value);
+const n2 = Number(document.getElementById("n2").value);
+
+let risultato;
+
+if (tipo === "somma") => n1 + n2; {
+    else if (tipo === "sottrazione") => n1 - n2 {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
