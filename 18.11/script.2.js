9195008d52e3f11d446cf91758210371ba5f3259
Riccardo V. <ricc.vargiu@gmail.com>
2025-11-18 12:39:03 +0100

    Add new HTML structure and JavaScript for DOM manipulation and element logging

---
+++ 18.11/script.2.js
@@ -0,0 +1,11 @@
+const tuttiGliA = document.querySelectorAll("a");
+console.log(tuttiGliA);
+
+const tuttiParagrafi = document.querySelectorAll("p");
+console.log(tuttiParagrafi.length);
+
+const tuttiDataId = document.querySelectorAll("[data-id]");
+console.log(tuttiDataId);
+
+const tuttiCancelletto = document.querySelectorAll("a[href='#']");
+console.log(tuttiCancelletto);
---
+++ 18.11/script.js
@@ -0,0 +1,42 @@
+// const cibo = document.querySelectorAll("li");
+// for (const robe of cibo) {
+//   console.log(robe.textContent);
+// }
+
+const cibo = document.querySelectorAll("li"); //creo una costante cibo e la associo a li
+for (const cose of cibo) {
+  // creo un'altra costante robe per ciclare cibo nel ciclo for of
+  console.log(cose.textContent); //cerco il textcontent di robe
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
