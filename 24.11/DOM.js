f3ae92a3316a33c13c43c869409aad20b1187fee
Riccardo V. <ricc.vargiu@gmail.com>
2025-11-24 17:14:50 +0100

    fix: esercitazione fetch ended

--- 24.11/DOM.js
+++ 24.11/DOM.js
@@ -1,21 +1,24 @@
-import { apiCall } from "./FETCH.JS";
+import { apiCall } from "./FETCH.js";
+
 const API_BASE_URL = "https://jsonplaceholder.typicode.com";
-const ul = document.getElementById(`ul`);
+const ul = document.getElementById("ul");
 
 async function fetchUsers() {
   const users = await apiCall(`${API_BASE_URL}/users`);
+
   if (users) {
     console.log("Dati Users ricevuti", users);
-  }
 
-  users.forEach((element) => {
-    const li = document.createElement(`li`);
-    li.innerHTML += `<div class="card">
-        <h3><strong>${element.name}</strong></h3>
-        <p><strong>Username:</strong> ${element.username}</p>
-        <p><strong>Email:</strong> ${element.email}</p>
+    users.forEach((element) => {
+      const li = document.createElement("li");
+      li.innerHTML = `<div class="card">
+          <h3><strong>${element.name}</strong></h3>
+          <p><strong>Username:</strong> ${element.username}</p>
+          <p><strong>Email:</strong> ${element.email}</p>
         </div>`;
-    ul.appendChild(li);
-  });
+      ul.appendChild(li);
+    });
+  }
 }
+
 fetchUsers();
