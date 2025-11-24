8912ad47c5e4a57d1948a325a6a699be01de2585
Riccardo V. <ricc.vargiu@gmail.com>
2025-11-24 17:34:14 +0100

    docs: esercizi 24/11

---
+++ esercizi 24/sctript.js
@@ -0,0 +1,14 @@
+import { API_KEY } from "./11/keys.js";
+const url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: API_KEY,
+  },
+};
+
+fetch(url, options)
+  .then((res) => res.json())
+  .then((json) => console.log(json))
+  .catch((err) => console.error(err));
