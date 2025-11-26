1c823d3a3118c6874f7be1a8b38b924ddc15d079
Riccardo V. <ricc.vargiu@gmail.com>
2025-11-26 17:42:16 +0100

    refactor: remove unused Input component from App and clean up TodoItem component

--- ESERCIZI REACT/26/11/src/App.jsx
+++ ESERCIZI REACT/26/11/src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.css";
 import Checkbox from "./components/atoms/Checkbox/Checkbox.jsx";
-import Input from "./components/atoms/Input/Input.jsx";
 import TodoItem from "./components/molecules/TodoItems/TodoItem.jsx";
 
 function App() {
   return (
     <>
-    
       <Checkbox />
-      <Input />
       <TodoItem />
     </>
   );
--- ESERCIZI REACT/26/11/src/components/molecules/TodoItems/TodoItem.jsx
+++ ESERCIZI REACT/26/11/src/components/molecules/TodoItems/TodoItem.jsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
 import Button from "../../atoms/Button/Button";
-import "./TodoItems.css"
 import Input from "../../atoms/Input/Input";
+import "./TodoItems.css";
 
 function TodoItem() {
-const [count, setCount] = useState(0);
-count = value;
+  const [count, setCount] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
   return (
     <div>
-     
-  <h1 className="todo-item">To Do List</h1>
-  <Button onClick ={() => setCount(count + 1)}>Add</Button>
- <Button onClick={() => setCount(0)}>Reset</Button>
-</div>
-  )
+      <h1 className="todo-item">To Do List</h1>
+      <Input value={inputValue}
+      onChange={(e) => setInputValue(e.target.value)} />
+      <Button onClick={() => setCount(count + 1)}>Add</Button>
+      <Button onClick={() => setCount(0)}>Reset</Button>
+    </div>
+  );
 }
 
 export default TodoItem;
